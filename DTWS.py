77392b0cb12beece330a6ab068a4b1d9efc4a250
George <ethilios@protonmail.com>
2020-07-17 16:42:53 +0100

    Added Dog class and parsing helper function

--- DTWS.py
+++ DTWS.py
@@ -1,16 +1,37 @@
 import math, requests
 from bs4 import BeautifulSoup
 
+# URL includes the filter 'May live with... Dogs'
 DT_base_url = "https://www.dogstrust.org.uk/rehoming/dogs/filters/~~~~~n~~d/page/"
-# Use array of objects here would be better
-dog_list = []
 # Create initial page object to find total dogs - vars will be overwritten in loop
 current_page = requests.get(DT_base_url + "1")
 req_status = current_page.status_code
 soup = BeautifulSoup(current_page.content, 'html.parser')
 total_dogs = int(soup.find('strong').get_text())
-# Required to declare as int again due to math.ceil returning a float
+# Required to re-declare as int again due to math.ceil returning a float
 num_pages = int(math.ceil((total_dogs) / 12))
+dog_info_link = "#BodyContent_DogList1_rptDogList_lnkDog_"
+# Initialise list to hold all dogs' info
+all_dogs = []
+
+####################
+
+class Dog:
+    def __init__(self, name, breed, location):
+        self.name = name
+        self.breed = breed
+        self.location = location
+
+def parse_and_save(index):
+    dog_name = soup.select(dog_info_link + str(index) + "> h3")[0].text.strip()
+    dog_breed = soup.select(dog_info_link + str(index) + "> span")[0].text.strip()
+    dog_location = soup.select(dog_info_link + str(index) + "> span > strong")[0].text.strip()
+
+    # Populate new Dog object and add to all_dogs list
+    current_dog = Dog(dog_name, dog_breed, dog_location)
+    all_dogs.append(current_dog)
+
+    print("Saving... " + current_dog.name)
 
 print("Starting Scan...\n")
 
@@ -29,9 +50,7 @@ for i in range(1,num_pages):
     soup = BeautifulSoup(current_page.content, 'html.parser')
     # Loop through elements on each page
     for j in range(0,12):
-        current_el = soup.select("#BodyContent_DogList1_rptDogList_lnkDog_" + str(j) + "> h3")
-        dog_list += current_el[0].text.strip()
-        print("\t - " + current_el[0].text.strip())
+        parse_and_save(j)
 
 # Handle elements on last page
 last_page = requests.get(DT_base_url + str(num_pages))
@@ -39,6 +58,4 @@ soup = BeautifulSoup(last_page.content, 'html.parser')
 dogs_on_last_page = total_dogs % 12
 print("\nPAGE: " + str(num_pages))
 for i in range(0, dogs_on_last_page):
-    current_el = soup.select("#BodyContent_DogList1_rptDogList_lnkDog_" + str(i) + "> h3")
-    dog_list += current_el[0].text.strip()
-    print("\t - " + current_el[0].text.strip())
+    parse_and_save(i)
