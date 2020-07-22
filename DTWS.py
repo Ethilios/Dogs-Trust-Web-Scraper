5baf7c2d6d8a3fc93984b6d636b382734ae14c52
George <ethilios@protonmail.com>
2020-07-22 10:03:35 +0100

    Added extraction of dog profile link

--- DTWS.py
+++ DTWS.py
@@ -10,20 +10,21 @@ from bs4 import BeautifulSoup
 
 ###         VARIABLES       ###
 
+DT_base_url = "https://www.dogstrust.org.uk"
 # URL includes the filter 'May live with... Dogs'
-DT_base_url = "https://www.dogstrust.org.uk/rehoming/dogs/filters/~~~~~n~~d/page/"
+DT_filtered_page = DT_base_url + "/rehoming/dogs/filters/~~~~~n~~d/page/"
 # pylint: disable=anomalous-backslash-in-string
 old_CSV_data_path = "data\previous-dogs.csv"
 new_CSV_data_path = "data\current-dogs.csv"
 updates_csv_path = r"data\updates.csv"
 # Create initial page object to find total dogs - vars will be overwritten in loop
-current_page = requests.get(DT_base_url + "1")
+current_page = requests.get(DT_filtered_page + "1")
 req_status = current_page.status_code
 soup = BeautifulSoup(current_page.content, 'html.parser')
 total_dogs = int(soup.find('strong').get_text())
 # Required to re-declare as int again due to math.ceil returning a float
 num_pages = int(math.ceil((total_dogs) / 12))
-dog_info_link = "#BodyContent_DogList1_rptDogList_lnkDog_"
+dog_selector = "#BodyContent_DogList1_rptDogList_lnkDog_"
 # Initialise list to hold all dogs' info
 all_dogs = []
 
@@ -31,21 +32,26 @@ all_dogs = []
 ###       CLASSES & FUNCTIONS      ###
 
 class Dog:
-    def __init__(self, name, breed, location):
+    def __init__(self, name, breed, location, link):
         self.name = name
         self.breed = breed
         self.location = location
+        self.link = link
     
     def __iter__(self):
-        return iter([self.name, self.breed, self.location])
+        return iter([self.name, self.breed, self.location, self.link])
 
 def parse_and_save(index):
-    dog_name = soup.select(dog_info_link + str(index) + "> h3")[0].text.strip()
-    dog_breed = soup.select(dog_info_link + str(index) + "> span")[0].text.strip()
-    dog_location = soup.select(dog_info_link + str(index) + "> span > strong")[0].text.strip()
+    selector = dog_selector + str(index)
+
+    dog_name = soup.select(selector + "> h3")[0].text.strip()
+    dog_breed = soup.select(selector + "> span")[0].text.strip()
+    dog_location = soup.select(selector + "> span > strong")[0].text.strip()
+    relative_link = soup.select(selector)[0].attrs['href']
+    dog_link = DT_base_url + relative_link
 
     # Populate new Dog object and add to all_dogs list
-    current_dog = Dog(dog_name, dog_breed, dog_location)
+    current_dog = Dog(dog_name, dog_breed, dog_location, dog_link)
     all_dogs.append(current_dog)
 
     print("Saving... " + current_dog.name)
@@ -183,19 +189,19 @@ if (req_status == 200):
     print("Starting Scan...\n")
 
     print("Total dogs on site: " + str(total_dogs))
-    print("Pages: " + str(num_pages))
+    print("Pages: " + str(num_pages))    
 
     # Loop through pages - except last page - see below
     for i in range(1,num_pages):
         print("\nPAGE: " + str(i))
-        current_page = requests.get(DT_base_url + str(i))
+        current_page = requests.get(DT_filtered_page + str(i))
         soup = BeautifulSoup(current_page.content, 'html.parser')
         # Loop through elements on each page
         for j in range(0,12):
             parse_and_save(j)
 
     # Handle elements on last page
-    last_page = requests.get(DT_base_url + str(num_pages))
+    last_page = requests.get(DT_filtered_page + str(num_pages))
     soup = BeautifulSoup(last_page.content, 'html.parser')
     dogs_on_last_page = total_dogs % 12
     print("\nPAGE: " + str(num_pages))
